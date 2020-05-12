e35b516c5ef67804cbb7c9623634f3180d3ec346
Catalin <lungancatalin@yahoo.com>
2020-05-12 22:45:21 +0300

    Updates

---
+++ precache-manifest.c40cbbaaf7345ae29fc64dddc656c821.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb49b6a2538606cb2f444aaddaa672e",
+    "url": "/portfolio_dev/index.html"
+  },
+  {
+    "revision": "7159f8cd027102658c7c",
+    "url": "/portfolio_dev/static/css/main.2eeb1167.chunk.css"
+  },
+  {
+    "revision": "3be1fc9722affd44be3b",
+    "url": "/portfolio_dev/static/js/2.13c304e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_dev/static/js/2.13c304e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7159f8cd027102658c7c",
+    "url": "/portfolio_dev/static/js/main.d3ab9b4d.chunk.js"
+  },
+  {
+    "revision": "1d870ca09eb0c0abd73e",
+    "url": "/portfolio_dev/static/js/runtime-main.57a48f48.js"
+  },
+  {
+    "revision": "cb3a545077e60adb039c383829b7c08a",
+    "url": "/portfolio_dev/static/media/avatar.cb3a5450.jpg"
+  },
+  {
+    "revision": "2efbd548f11df1c5729c165b38595519",
+    "url": "/portfolio_dev/static/media/base.2efbd548.png"
+  },
+  {
+    "revision": "1b4983d587b0a75190ba86011eafda85",
+    "url": "/portfolio_dev/static/media/cards.1b4983d5.png"
+  },
+  {
+    "revision": "fe663dc33021423a478baecbdb1e5e42",
+    "url": "/portfolio_dev/static/media/clipboard.fe663dc3.png"
+  },
+  {
+    "revision": "cb09bdaed43e14d8a3df109eec54d642",
+    "url": "/portfolio_dev/static/media/covid19.cb09bdae.jpg"
+  },
+  {
+    "revision": "07bf22632e2677dc4c26795ce557c469",
+    "url": "/portfolio_dev/static/media/dentist_demo.07bf2263.png"
+  },
+  {
+    "revision": "ae74261d1c1175a278a0d6ce4034ca4e",
+    "url": "/portfolio_dev/static/media/frontend_img.ae74261d.svg"
+  },
+  {
+    "revision": "772939db63ee56152214a0a2cda730ac",
+    "url": "/portfolio_dev/static/media/fylo.772939db.png"
+  },
+  {
+    "revision": "6bed8f55572dd6a07dd429c0ce5e4232",
+    "url": "/portfolio_dev/static/media/huddle.6bed8f55.png"
+  },
+  {
+    "revision": "959040f59a63ceb5a435c8c4a52f6a49",
+    "url": "/portfolio_dev/static/media/icon-facebook.959040f5.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/portfolio_dev/static/media/icon-github.1bb02710.svg"
+  },
+  {
+    "revision": "341a67c538d67f9ce92005cf14255dc2",
+    "url": "/portfolio_dev/static/media/icon-instagram.341a67c5.svg"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/portfolio_dev/static/media/icon-linkedin.5b238434.svg"
+  },
+  {
+    "revision": "ee4877d4f950148f64daf483d2b3464c",
+    "url": "/portfolio_dev/static/media/insure.ee4877d4.jpg"
+  },
+  {
+    "revision": "425c2b01b4c091e36fb9e8c35a95215f",
+    "url": "/portfolio_dev/static/media/logo-white.425c2b01.svg"
+  },
+  {
+    "revision": "425c2b01b4c091e36fb9e8c35a95215f",
+    "url": "/portfolio_dev/static/media/logo.425c2b01.svg"
+  },
+  {
+    "revision": "5dc2f60fa456377d3c949250b2a2d2d9",
+    "url": "/portfolio_dev/static/media/project_tracking.5dc2f60f.png"
+  },
+  {
+    "revision": "9df29fe2d7988782edae882ea7749834",
+    "url": "/portfolio_dev/static/media/psd_to_html.9df29fe2.svg"
+  },
+  {
+    "revision": "253b841c5101e5e4410573d6f1a2ffbe",
+    "url": "/portfolio_dev/static/media/responsive_design.253b841c.svg"
+  },
+  {
+    "revision": "cba2b2b003eb111b063a8718aca7af0c",
+    "url": "/portfolio_dev/static/media/single_price.cba2b2b0.png"
+  },
+  {
+    "revision": "056096fc4cb24afc06b8ce610af030a1",
+    "url": "/portfolio_dev/static/media/support.056096fc.svg"
+  },
+  {
+    "revision": "64318b190eb0c3fcaf85635243344b5a",
+    "url": "/portfolio_dev/static/media/web_development.64318b19.svg"
+  },
+  {
+    "revision": "c85bc38922edbd5ce0fee8f6577037b4",
+    "url": "/portfolio_dev/static/media/wordpress.c85bc389.svg"
+  }
+]);
